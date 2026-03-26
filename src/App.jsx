193422f38180907f04b6459bbc31deb053567e5c
divietrodenny-commits <divietrodenny@gmail.com>
2026-03-26 11:49:28 +0100

    aggiunto input e button + fetch

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,33 @@ import './App.css'
 function App() {
 
   const api_key = import.meta.env.VITE_API_KEY
-  console.log(api_key);
-  const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=ritorno+al+futuro`
-console.log(api_url);
+  
+  
+  const [query, setQuery] = useState('')
+  const [movies, setMovies] = useState([])
+  
+  function searchMovies() {
+    const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=ritorno+al+futuro`
 
+    fetch(api_url)
+      .then(res => res.json())
+      .then(data => {
+        console.log([])
+        setMovies(data)
+      })
+      .catch(err => console.error('Errore API:', err))
+  }
 
   return (
     <>
+      <div>
+        <input
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="cerca film"/>
+        <button onClick={searchMovies}>Cerca</button>
 
+      </div>
     </>
   )
 }
