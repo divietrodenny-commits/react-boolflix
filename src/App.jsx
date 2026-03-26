9f03be0b2343690b39f5c34f359d2f6e42c15285
divietrodenny-commits <divietrodenny@gmail.com>
2026-03-26 11:01:17 +0100

    sistemato url

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   const api_key = import.meta.env.VITE_API_KEY
   console.log(api_key);
-  const api_url = `https://api.themoviedb.org/3/search/movie?api_key=d86f088fddafc60410d1e2d7615b7fe1&query=ritorno+al+fut
+  const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}=ritorno+al+fut
 uro`
 console.log(api_url);
 
