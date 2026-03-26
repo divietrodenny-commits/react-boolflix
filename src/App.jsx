029cae3c688b2de6617b43b0b757f12c88f57eb0
divietrodenny-commits <divietrodenny@gmail.com>
2026-03-26 12:34:46 +0100

    Aggiunta funzione per le flag + <p> nel return

--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,47 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
   const api_key = import.meta.env.VITE_API_KEY
   const [query, setQuery] = useState('')
   const [movies, setMovies] = useState([])
 
+  function getFlag(language) {
+    const flags = {
+      it: '🇮🇹',
+      en: '🇺🇸',
+      fr: '🇫🇷'
+    }
+    return flags[language] || 'Lingua non trovata'
+  }
+
   function searchMovies() {
     const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${encodeURIComponent(query)}&language=it-IT`
-
     fetch(api_url)
       .then(res => res.json())
-      .then(data => {
-        console.log([])
-        setMovies(data.results)
-      })
+      .then(data => setMovies(data.results || []))
       .catch(err => console.error('Errore API:', err))
   }
 
   return (
     <>
-      <div>
-        <input
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder="Cerca un film" />
-        <button onClick={searchMovies}>Cerca</button>
-        <ul>
-          {movies.slice(0, 5).map(movie => (
-            <li key={movie.id}>
-              <h3>{movie.title}</h3>
-              <p>{movie.original_title}</p>
-              <p>{movie.original_language}</p>
-              <p>{movie.vote_average}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+    <div>
+      <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Cerca un film"/>
+      <button onClick={searchMovies}>Cerca</button>
+
+      <ul>
+        {movies.slice(0, 5).map(movie => (
+          <li key={movie.id}>
+            <h3>{movie.title}</h3>
+            <p>{movie.original_title}</p>
+            <p>{getFlag(movie.original_language)} {movie.original_language}</p>
+            <p>{movie.vote_average}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
     </>
   )
 }
