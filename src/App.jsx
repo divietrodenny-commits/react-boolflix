64c65fc5f0c219ad10f3999b7b29f80e133b841f
divietrodenny-commits <divietrodenny@gmail.com>
2026-03-26 12:09:38 +0100

    sistemato api_url ed aggiunto ul dinamico

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,17 @@ import './App.css'
 function App() {
 
   const api_key = import.meta.env.VITE_API_KEY
-  
-  
   const [query, setQuery] = useState('')
   const [movies, setMovies] = useState([])
-  
+
   function searchMovies() {
-    const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=ritorno+al+futuro`
+    const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${encodeURIComponent(query)}&language=it-IT`
 
     fetch(api_url)
       .then(res => res.json())
       .then(data => {
         console.log([])
-        setMovies(data)
+        setMovies(data.results)
       })
       .catch(err => console.error('Errore API:', err))
   }
@@ -27,9 +25,18 @@ function App() {
         <input
           value={query}
           onChange={(e) => setQuery(e.target.value)}
-          placeholder="cerca film"/>
+          placeholder="Cerca un film" />
         <button onClick={searchMovies}>Cerca</button>
-
+        <ul>
+          {movies.map(movie => (
+            <li key={movie.id}>
+              <h3>{movie.title}</h3>
+              <p>{movie.original_title}</p>
+              <p>{movie.original_language}</p>
+              <p>{movie.vote_average}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   )
